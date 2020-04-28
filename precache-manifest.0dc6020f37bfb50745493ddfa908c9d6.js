66e2a30a55a3db773ed023e020496394d330e4a1
Taras Golyk <tarasgolyk87@gmail.com>
2020-04-28 17:25:43 +0300

    Updates

---
+++ precache-manifest.0dc6020f37bfb50745493ddfa908c9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac9652687cee3b241095c3aa2e84221",
+    "url": "/react-typescript-todolist/index.html"
+  },
+  {
+    "revision": "3c9a088a884113b10d00",
+    "url": "/react-typescript-todolist/static/css/main.727ee21e.chunk.css"
+  },
+  {
+    "revision": "80cd0d3d2e697c92e8b6",
+    "url": "/react-typescript-todolist/static/js/2.11595caf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-typescript-todolist/static/js/2.11595caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9a088a884113b10d00",
+    "url": "/react-typescript-todolist/static/js/main.f7697003.chunk.js"
+  },
+  {
+    "revision": "e433f627b7752b2a35f7",
+    "url": "/react-typescript-todolist/static/js/runtime-main.4ffacd22.js"
+  }
+]);
